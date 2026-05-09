3c44cb5fb6d8d701e00f87dd63a6a7ed9ff27949
unknown <gustavocastruu@gmail.com>
2026-05-08 21:42:25 -0300

    Reforçando as bibliotecas numpy , pandas e matploty

---
+++ bibliotecas_dados/001.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+# array = np.array([1,2,3,4,5])
+# print(array)
+
+# dados = {"nome":["Gustavo"],"sobrenome":["Castro"],"Cargo": ["Analista de Faturamento PL"]}
+
+# data = pd.DataFrame(dados)
+# data.to_excel("dados.xlsx")
+
+# y = [10,20,30,40,50]
+# x = [100,200,300,400,500]
+
+# plt.plot(y,x)
+# plt.show()
+
+
+arrays = np.array([10,20,30,40])
+resultado = arrays * 2 
+print(resultado)
